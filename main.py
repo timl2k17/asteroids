fe5cd798094f853f3ad65ea77929d63806d5a100
Tim LaDuca <tjl@me.com>
2025-05-14 19:52:48 -0700

    using groups for real

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        updateable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
